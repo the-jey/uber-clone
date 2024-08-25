6770b749b2b9dd944284539a2b8e675eb7afb03b
the-jey <jey.pley@gmail.com>
2024-08-25 20:20:34 +0200

    welcome screen with swiper

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,4 @@
 import { Redirect } from "expo-router";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 const Index = () => {
   return <Redirect href="/(auth)/welcome" />
