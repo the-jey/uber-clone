aa2a9c31951dc9a6f959b489f219c0abcaf4460c
the-jey <jey.pley@gmail.com>
2024-08-25 18:41:49 +0200

    project structure setup

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Redirect } from "expo-router";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Index = () => {
+  return <Redirect href="/(auth)/welcome" />
+}
+
+export default Index;
