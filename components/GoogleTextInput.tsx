6c6eaa6e538ed6d2c0c0a5697063c64b9a158ac0
the-jey <jey.pley@gmail.com>
2024-08-28 16:56:45 +0200

    location autocomplete feature

--- components/GoogleTextInput.tsx
+++ components/GoogleTextInput.tsx
@@ -1,5 +1,10 @@
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+
 import { GoogleInputProps } from '@/types/type'
-import { View, Text } from 'react-native'
+import { Image, View } from 'react-native'
+import { icons } from "@/constants";
+
+const googlePlacesApiKey = process.env.EXPO_PUBLIC_GOOLE_API_KEY;
 
 const GoogleTextInput = ({
   icon,
@@ -10,7 +15,56 @@ const GoogleTextInput = ({
 }: GoogleInputProps) => {
   return (
     <View className={`flex flex-row items-center justify-center relative z-50 rounded-xl ${containerStyle} mb-5`}>
-      <Text>Search</Text>
+      <GooglePlacesAutocomplete
+        fetchDetails={true}
+        placeholder="Where you want to go?"
+        debounce={200}
+        styles={{
+          textInputContainer: {
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 20,
+            marginHorizontal: 20,
+            position: 'relative',
+            shadowColor: '#D4D4D4'
+          },
+          textInput: {
+            backgroundColor: textInputBackgroundColor || "white",
+            fontSize: 16,
+            fontWeight: "600",
+            marginTop: 5,
+            width: "100%",
+            borderRadius: 200,
+          },
+          listView: {
+            backgroundColor: textInputBackgroundColor || "white",
+            position: "relative",
+            top: 0,
+            width: "100%",
+            borderRadius: 10,
+            shadowColor: "#D4D4D4",
+            zIndex: 99,
+          }
+        }}
+        onPress={(data, details = null) => handlePress({
+          latitude: details?.geometry.location.lat!,
+          longitude: details?.geometry.location.lng!,
+          address: data?.description
+        })}
+        query={{
+          key: googlePlacesApiKey,
+          language: "fr"
+        }}
+        renderLeftButton={() => (
+          <View className="justify-center items-center w-6 h-6">
+            <Image source={icon ? icon : icons.search} className="w-6 h-6" resizeMode="contain" />
+          </View>
+        )}
+        textInputProps={{
+          placeholderTextColor: 'gray',
+          placeholder: initialLocation ?? "Where do you want to go?"
+        }}
+      />
     </View>
   )
 }
